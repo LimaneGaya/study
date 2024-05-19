abb107a696f00a38d8d4277d397669ff91f46e5c
Gaya <limanegaya@gmail.com>
2024-05-19 15:01:29 +0100

    Simple Brick Breaker Game

---
+++ _19_05_2024_brick_breaker/README.md
@@ -0,0 +1,3 @@
+# _19_05_2024_brick_breaker
+
+Project following [Google Codelab](https://codelabs.developers.google.com/codelabs/flutter-flame-brick-breaker)
---
+++ _19_05_2024_brick_breaker/android/app/src/main/kotlin/com/example/u_19_05_2024_brick_breaker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u_19_05_2024_brick_breaker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
