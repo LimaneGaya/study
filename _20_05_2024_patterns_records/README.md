ff382d7b1f83ba13db8e9516e2edea3da7c39f20
Gaya <limanegaya@gmail.com>
2024-05-20 21:11:02 +0100

    Records and Patterns

---
+++ _20_05_2024_patterns_records/README.md
@@ -0,0 +1,3 @@
+# _20_05_2024_patterns_records
+
+Project following [Google Codelab](https://codelabs.developers.google.com/codelabs/dart-patterns-records)
---
+++ _20_05_2024_patterns_records/android/app/src/main/kotlin/com/example/u_20_05_2024_patterns_records/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u_20_05_2024_patterns_records
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
