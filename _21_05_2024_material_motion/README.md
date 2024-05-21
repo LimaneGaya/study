539bb5b4beb7155ed044df5ffe56ffcfefaf3b11
Gaya <limanegaya@gmail.com>
2024-05-21 21:49:57 +0100

    Material Motion Transitions

---
+++ _21_05_2024_material_motion/README.md
@@ -0,0 +1,3 @@
+# _21_05_2024_material_motion
+
+Following [Codelab](https://codelabs.developers.google.com/codelabs/material-motion-flutter)
---
+++ _21_05_2024_material_motion/android/app/src/main/kotlin/com/example/u_21_05_2024_material_motion/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u_21_05_2024_material_motion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
