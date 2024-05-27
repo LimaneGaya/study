6c03dd302722027dadd2e9b582a6cf5d466a93ae
Gaya <limanegaya@gmail.com>
2024-05-27 17:42:49 +0100

    github_client

---
+++ 27_05_2024_github_client/README.md
@@ -0,0 +1,3 @@
+# github_client
+
+Following [Codelab](https://codelabs.developers.google.com/codelabs/flutter-github-client)
---
+++ 27_05_2024_github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+const githubClientId = '###########';
+const githubClientSecret = '########################';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
