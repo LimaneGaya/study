51cf1062ecda6edb75281e4e83ef55bdfa3c0135
Gaya <limanegaya@gmail.com>
2024-08-30 22:15:06 +0100

    intro to process state persistance using files

--- _2024_08_22_elixir/todo_cache/lib/todo/database.ex
+++ _2024_08_22_elixir/todo_cache/lib/todo/database.ex
@@ -1,7 +1,8 @@
 defmodule Todo.Database do
   use GenServer
 
-  @db_folder "./persist"
+  @db_folder "./persistElixirProject"
+
   def start do
     GenServer.start(__MODULE__, nil, name: __MODULE__)
   end
@@ -9,27 +10,39 @@ defmodule Todo.Database do
   def store(key, data) do
     GenServer.cast(__MODULE__, {:store, key, data})
   end
+
   def get(key) do
     GenServer.call(__MODULE__, {:get, key})
   end
+
+  @impl GenServer
   def init(_) do
     File.mkdir_p!(@db_folder)
     {:ok, nil}
   end
+
+  @impl GenServer
   def handle_cast({:store, key, data}, state) do
-    key
-    |> file_name()
-    |> File.write!(:erlang.term_to_binary(data))
-    {:noreply,state}
+    spawn(fn ->
+        key
+        |> file_name()
+        |> File.write!(:erlang.term_to_binary(data))
+      end)
+    {:noreply, state}
   end
 
-  def handle_call({:get, key}, _, state) do
-    data = case File.read(file_name(key)) do
-      {:ok, contents} -> :erlang.binary_to_term(contents)
-      _ -> nil
-    end
-    {:reply, data, state}
+  @impl GenServer
+  def handle_call({:get, key}, caller, state) do
+    spawn(fn ->
+        data = case File.read(file_name(key)) do
+          {:ok, contents} -> :erlang.binary_to_term(contents)
+          _ -> nil
+        end
+        GenServer.reply(caller, data)
+      end)
+      {:noreply, state}
   end
+
   defp file_name(key) do
     Path.join(@db_folder, to_string(key))
   end
