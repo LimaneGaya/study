d5c4dbb6d0d8bb7cfcdc35d2d87f43916f72c47c
Gaya <limanegaya@gmail.com>
2024-09-10 22:33:41 +0100

    introduction to cowboy and plub, implemented a simple web interface

--- _2024_08_22_elixir/supervised_todo_cache/test/cache_test.exs
+++ _2024_08_22_elixir/supervised_todo_cache/test/cache_test.exs
@@ -1,11 +1,10 @@
 defmodule Todo.CacheTest do
   use ExUnit.Case
   test "server_process" do
-    {:ok, cache} = Todo.Cache.start()
-    bob_pid = Todo.Cache.server_process(cache, "bob")
-    assert bob_pid != Todo.Cache.server_process(cache, "alice")
-    assert bob_pid == Todo.Cache.server_process(cache, "bob")
-  end
+    bob_pid = Todo.Cache.server_process("bob")
+    assert bob_pid != Todo.Cache.server_process("alice")
+    assert bob_pid == Todo.Cache.server_process("bob")
+    end
   test "to-do operations" do
     {:ok, cache} = Todo.Cache.start()
     alice = Todo.Cache.server_process(cache, "alice")
