7323eadc6b63c6f3d741c69853459d4faf6e8142
Gaya <limanegaya@gmail.com>
2024-09-13 15:39:36 +0100

    finished setting environement using runtime and config, setup different port and db location for mix environement

--- _2024_08_22_elixir/supervised_todo_cache/config/runtime.exs
+++ _2024_08_22_elixir/supervised_todo_cache/config/runtime.exs
@@ -1,4 +1,14 @@
 import Config
 
-http_port = System.get_env("TODO_HTTP_PORT", "5454")
+http_port =
+  if config_env() != :test,
+    do: System.get_env("TODO_HTTP_PORT", "5454"),
+    else: System.get_env("TODO_TEST_HTTP_PORT", "5455")
+
 config :todo, http_port: String.to_integer(http_port)
+
+database_folder =
+  if config_env() != :test,
+    do: System.get_env("DATABASE_FOLDER", "database"),
+    else: System.get_env("DATABASE_TEST_FOLDER", "database_test")
+config :todo, :database, db_folder: database_folder
