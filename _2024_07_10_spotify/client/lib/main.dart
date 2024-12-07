ddb52f585b72cef08e53250ac93b99d097713874
Gaya <limanegaya@gmail.com>
2024-12-07 19:57:16 +0100

    Removed unfinished Spotify clone and moved it into a new personal project with different tech stack

--- _2024_07_10_spotify/client/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'service_locator.dart';
-import 'src/app.dart';
-
-void main() async {
-  await setupServiceLocator();
-  runApp(MyApp(settingsController: getit()));
-}
--- _2024_07_10_spotify/client/lib/src/features/auth/presentation/cubit/auth_state.dart
+++
@@ -1,7 +0,0 @@
-part of 'auth_cubit.dart';
-
-abstract class AuthState {
-  const AuthState();
-}
-
-class AuthInitial extends AuthState {}
